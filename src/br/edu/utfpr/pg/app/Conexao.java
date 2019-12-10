0af0376e58244ea8188f9aa80608a26709ed09f9
Vinícius Camargo Andrade <vinicius.andrade1990@gmail.com>
2019-12-10 15:25:43 -0200

    v. 1.0 - MySQL database connection and test class

---
+++ src/br/edu/utfpr/pg/app/Conexao.java
@@ -0,0 +1,37 @@
+package br.edu.utfpr.pg.app;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * @author Vinicius 
+ * @UTFPR
+ */
+
+public class Conexao {
+
+    private static Connection conn;
+    private final String driver;
+    private final String usuario;
+    private final String senha;
+    private final String url;
+
+    public Conexao() {
+        
+        this.driver = "com.mysql.jdbc.Driver";
+        this.url = "jdbc:mysql://localhost:3306/basedadosaula";
+        this.usuario = "root";
+        this.senha = "";
+    }
+
+    public Connection conectar() throws ClassNotFoundException, SQLException {
+        
+        this.conn = null;
+        Class.forName(driver);
+        this.conn = DriverManager.getConnection(url, usuario, senha);        
+        
+        return this.conn;
+    }
+}
+
---
+++ test/br/edu/utfpr/pg/app/ConexaoTest.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.edu.utfpr.pg.app;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * @author Vinicius
+ * @UTFPR
+ */
+
+public class ConexaoTest {
+
+    private Conexao conexao;
+    private Connection conn;
+    
+    @Before
+    public void setUp() {
+    
+        this.conexao = new Conexao();
+    }
+
+    @After
+    public void tearDown() throws ClassNotFoundException, SQLException {
+    
+        conn.close();
+    }
+
+    @Test
+    public void conectarTest() throws ClassNotFoundException, SQLException {
+    
+        this.conn = conexao.conectar();        
+        assertNotNull(this.conn);
+    }
+}
